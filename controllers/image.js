4282e5564dae8dbcadf586ca1273a367d98b6b59
Pavlo Vasylkivsliy <pavel.vasylkivskiy@gmail.com>
2019-04-07 20:26:37 +0200

    changed connction settings

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => res.json(data))
-    .catch(err => res.status(400).json('unable to work with API'));ß
+    .catch(err => res.status(400).json('unable to work with API'));
 }
 
 const imageHandler = (req, res, db) => {
